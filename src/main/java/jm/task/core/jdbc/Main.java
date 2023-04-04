f12594635778c695c70928f8d8de899c8b0ce386
AlexRefery <vip.alx@bk.ru>
2023-04-04 20:11:54 +0300

    Add jdbc implementation

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,42 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+
+        UserService service = new UserServiceImpl();
+        service.createUsersTable();
+        service.saveUser("Max", "Lionov", (byte) 25);
+        service.saveUser("Ivan", "Block", (byte) 44);
+        service.saveUser("Helen", "Muromova", (byte) 18);
+        service.saveUser("Alex", "Ya", (byte) 35);
+
+
+
+        List<User> users = service.getAllUsers();
+
+        for (User user : users) {
+            System.out.println(user);
+        }
+
+//        service.removeUserById(4);
+//        System.out.println();
+//        users = service.getAllUsers();
+//
+//        for (User user : users) {
+//            System.out.println(user);
+//        }
+
+
+
+        service.cleanUsersTable();
+        service.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,112 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
+    private static final String CREATE_TABLE =
+            "CREATE TABLE IF NOT EXISTS users (" +
+                    "    id INT8 PRIMARY KEY GENERATED ALWAYS AS IDENTITY, " +
+                    "    name VARCHAR(100) NOT NULL, " +
+                    "    lastname VARCHAR(100) NOT NULL, " +
+                    "    age SMALLINT NOT NULL" +
+                    ");";
+    private static final String DROP_TABLE = "DROP TABLE IF EXISTS users CASCADE;";
+    private static final String SAVE_QUERY = "INSERT INTO users (name, lastname, age) VALUES(?, ?, ?);";
+    private static final String SELECT = "SELECT id, name, lastname, age FROM users ORDER BY name, lastname";
+    private static final String CLEAN_TABLE = "TRUNCATE TABLE users RESTART IDENTITY";
+    private static final String DELETE_ONE = "DELETE FROM users WHERE id=?";
+
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
+        try(Connection connection = Util.getConnection();
+            Statement statement = connection.createStatement()) {
 
+            statement.executeUpdate(CREATE_TABLE);
+        } catch (SQLException e) {
+            System.out.println("При создании таблицы что-то пошло не так!");
+            throw new RuntimeException(e);
+        }
     }
 
     public void dropUsersTable() {
+        try(Connection connection = Util.getConnection();
+            Statement statement = connection.createStatement()) {
 
+            statement.executeUpdate(DROP_TABLE);
+        } catch (SQLException e) {
+            System.out.println("При удалении таблицы что-то пошло не так!");
+            throw new RuntimeException(e);
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
+        try(Connection connection = Util.getConnection();
+            PreparedStatement statement = connection.prepareStatement(SAVE_QUERY, PreparedStatement.RETURN_GENERATED_KEYS)) {
 
+            statement.setString(1, name);
+            statement.setString(2, lastName);
+            statement.setInt(3, age);
+            statement.executeUpdate();
+            ResultSet rs = statement.getGeneratedKeys();
+            if (rs.next()) {
+                System.out.println("Создали юзера с номером id= " + rs.getLong("id"));
+            }
+
+        } catch (SQLException e) {
+            System.out.println("При записи юзера что-то пошло не так!");
+            throw new RuntimeException(e);
+        }
     }
 
     public void removeUserById(long id) {
+        try(Connection connection = Util.getConnection();
+            PreparedStatement statement = connection.prepareStatement(DELETE_ONE)) {
 
+            statement.setLong(1, id);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("При удалении юзера что-то пошло не так!");
+            throw new RuntimeException(e);
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> users = new ArrayList<>();
+        try(Connection connection = Util.getConnection();
+            PreparedStatement statement = connection.prepareStatement(SELECT);
+            ResultSet resultSet = statement.executeQuery()) {
+
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getLong("id"));
+                user.setName(resultSet.getString("name"));
+                user.setLastName(resultSet.getString("lastname"));
+                user.setAge(resultSet.getByte("age"));
+                users.add(user);
+            }
+        } catch (SQLException e) {
+            System.out.println("При чтении списка юзеров что-то пошло не так!");
+            throw new RuntimeException(e);
+        }
+        return users;
     }
 
     public void cleanUsersTable() {
+        try(Connection connection = Util.getConnection();
+            Statement statement = connection.createStatement()) {
 
+            statement.executeUpdate(CLEAN_TABLE);
+        } catch (SQLException e) {
+            System.out.println("При очистке таблицы что-то пошло не так!");
+            throw new RuntimeException(e);
+        }
     }
 }
